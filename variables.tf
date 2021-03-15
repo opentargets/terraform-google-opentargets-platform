d41911e337f25d9911790cdc45004ac27a5b9be1
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-15 13:09:10 +0000

    default GCP zone to use

--- variables.tf
+++ variables.tf
@@ -8,3 +8,7 @@ variable "config_gcp_default_region" {
   description = "Default region when not specified in the module"
 }
 
+variable "config_gcp_default_zone" {
+  description = "Default zone when not specified in the module"
+}
+
