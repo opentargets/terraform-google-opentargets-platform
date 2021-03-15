dda0949d521ca1ad088d5521f00096e8b282c720
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-15 13:08:43 +0000

    default GCP region to use

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@
 variable "config_release_name" {
   description = "Open Targets Platform release name, not related to any configuration parameter."
 }
+
+// --- DEPLOYMENT CONFIGURATION --- //
+variable "config_gcp_default_region" {
+  description = "Default region when not specified in the module"
+}
+
