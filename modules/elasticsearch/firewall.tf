59c2e97cfa4c063d387c308294e48800887cd1be
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 10:39:48 +0000

    module firewall config, allow elastic search requests

--- modules/elasticsearch/firewall.tf
+++ modules/elasticsearch/firewall.tf
@@ -1,3 +1,17 @@
 // --- Firewall Configuration --- //
 // Elastic Search Traffic --- //
 // Requests
+resource "google_compute_firewall" "vpc_netfw_elasticsearch_requests" {
+  name = "${var.network_name}-${var.deployment_region}-allow-elasticsearch-requests"
+  description = "Firewall rule for allowing Elastic Search Requests Traffic"
+  network = var.network_self_link
+
+  allow {
+      protocol = "tcp"
+      ports = [ local.elastic_search_port_requests ]
+  }
+
+  target_tags = [ local.fw_tag_elasticsearch_requests ]
+  source_ranges = var.network_source_ranges
+}
+
