d559256c70c054202d6d8322a3e23986a64f82dd
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 12:53:16 +0000

    clickhouse module, output information, general deployment information

--- modules/clickhouse/outputs.tf
+++ modules/clickhouse/outputs.tf
@@ -1,1 +1,13 @@
 // --- Module Output Information --- //
+output "deployment_region" {
+  value = var.deployment_region
+}
+
+output "network_name" {
+  value = var.network_name
+}
+
+output "network_subnet_name" {
+  value = var.network_subnet_name
+}
+
