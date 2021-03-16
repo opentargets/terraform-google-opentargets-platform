f18daaa582dda10969bbd4051bb01221f493ccda
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 10:05:39 +0000

    module input parameters, general section, network name

--- modules/elasticsearch/variables.tf
+++ modules/elasticsearch/variables.tf
@@ -5,3 +5,8 @@ variable "module_wide_prefix_scope" {
   default = "otpdeves"
 }
 
+variable "network_name" {
+  description = "Name of the network where resources should be deployed, 'default'"
+  default = "default"
+}
+
