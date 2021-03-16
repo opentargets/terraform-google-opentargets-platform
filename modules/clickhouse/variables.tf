bb85d2fb905de109dea74e152700bc5be7269963
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 12:13:33 +0000

    module clickhouse, input parameters, general deployment information, vpc information

--- modules/clickhouse/variables.tf
+++ modules/clickhouse/variables.tf
@@ -5,3 +5,23 @@ variable "module_wide_prefix_scope" {
   default = "otpdevch"
 }
 
+variable "network_name" {
+  description = "Name of the network resources will be connected to, default 'default'"
+  default = "default"
+}
+
+variable "network_self_link" {
+  description = "Self link to the network where resources should be connected when deployed, default 'default'"
+  default = "default"
+}
+
+variable "network_subnet_name" {
+  description = "Name of the subnet, within the 'network_name', and the given region, where instances should be connected to, default 'main-subnet'"
+  default = "main-subnet"
+}
+
+variable "network_source_ranges" {
+  description = "CIDR that represents which IPs we want to grant access to the deployed resources, default '10.0.0.0/9'"
+  default = [ "10.0.0.0/9" ]
+}
+
