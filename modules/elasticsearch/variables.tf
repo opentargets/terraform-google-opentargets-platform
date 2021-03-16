c4c7d3fd8831cd08cd5671061a6c96c632bbe13f
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 10:05:29 +0000

    module input parameters, general section, scope prefix

--- modules/elasticsearch/variables.tf
+++ modules/elasticsearch/variables.tf
@@ -1,2 +1,7 @@
 // --- Module input parameters --- //
 // General deployment input parameters --- //
+variable "module_wide_prefix_scope" {
+  description = "The prefix provided here will scope names for those resources created by this module, default 'otpdeves'"
+  default = "otpdeves"
+}
+
