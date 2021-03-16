05d7961b2b45e670fc12cf296abd05f7145b1782
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 11:10:06 +0000

    locals, port names

--- modules/elasticsearch/locals.tf
+++ modules/elasticsearch/locals.tf
@@ -2,6 +2,8 @@ locals {
   // Ports
   elastic_search_port_requests  = 9200
   elastic_search_port_comms     = 9300
+  elastic_search_port_requests_name = "esportrequests"
+  elastic_search_port_comms_name = "esportcomms"
   // Firewall tags
   fw_tag_elasticsearch_requests = "elasticsearchrequests"
   fw_tag_elasticsearch_comms = "elasticsearchcomms"
