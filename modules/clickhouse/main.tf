fc751ed8ed90ceb005b5f4bdc66cd6c0a7cf5c37
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 12:33:49 +0000

    module main definition, description

---
+++ modules/clickhouse/main.tf
@@ -0,0 +1,7 @@
+// Clickhouse Deployment for Open Target Platform
+// Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+
+/*
+    This module defines a regional Clickhouse deployment behind an ILB
+*/
+
