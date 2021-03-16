16191e3d6c57b51055b9fce17f36ded6658497f2
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 11:16:57 +0000

    elastic search module outputs, including named ports

--- modules/elasticsearch/outputs.tf
+++ modules/elasticsearch/outputs.tf
@@ -14,3 +14,20 @@ output "network_subnet_name" {
 output "ilb_ip_address" {
   value = google_compute_forwarding_rule.ilb_forwarding_rule.ip_address
 }
+
+// Named ports --- //
+output "port_elastic_search_requests" {
+  value = local.elastic_search_port_requests
+}
+
+output "port_elastic_search_requests_name" {
+  value = local.elastic_search_port_requests_name
+}
+
+output "port_elastic_search_comms" {
+  value = local.elastic_search_port_comms
+}
+
+output "port_elastic_search_comms_name" {
+  value = local.elastic_search_port_comms_name
+}
