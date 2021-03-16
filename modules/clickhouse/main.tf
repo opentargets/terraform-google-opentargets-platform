eb8b632190b123aa804fde92b342a27efefe0f39
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 12:36:14 +0000

    module main definition, randomizer for clickhouse VM template name

--- modules/clickhouse/main.tf
+++ modules/clickhouse/main.tf
@@ -5,3 +5,18 @@
     This module defines a regional Clickhouse deployment behind an ILB
 */
 
+// --- Machine Template --- //
+// TODO - Refactor using
+//      https://github.com/terraform-google-modules/terraform-google-vm
+resource "random_string" "random" {
+  length = 8
+  lower = true
+  upper = false
+  special = false
+  keepers = {
+    clickhouse_template_tags = join("", sort(local.clickhouse_template_tags)),
+    clickhouse_template_machine_type = local.clickhouse_template_machine_type,
+    clickhouse_template_source_image = local.clickhouse_template_source_image
+  }
+}
+
