45ed9d29666df8388fe00e018acfaa00b2f23aa7
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 10:40:04 +0000

    module firewall config, allow elastic search inter-node communications

--- modules/elasticsearch/firewall.tf
+++ modules/elasticsearch/firewall.tf
@@ -15,3 +15,17 @@ resource "google_compute_firewall" "vpc_netfw_elasticsearch_requests" {
   source_ranges = var.network_source_ranges
 }
 
+// Node communications
+resource "google_compute_firewall" "vpc_netfw_elasticsearch_comms" {
+  name = "${var.network_name}-${var.deployment_region}-allow-elasticsearch-comms"
+  description = "Firewall rule for allowing Elastic Search Nodes Traffic in a cluster"
+  network = var.network_self_link
+
+  allow {
+      protocol = "tcp"
+      ports = [ local.elastic_search_port_comms ]
+  }
+
+  target_tags = [ local.fw_tag_elasticsearch_comms ]
+  source_ranges = var.network_source_ranges
+}
