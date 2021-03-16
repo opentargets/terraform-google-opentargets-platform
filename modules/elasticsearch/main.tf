83a4b0e660de6c87bc1a8c82bf623e65f0e32876
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 10:54:12 +0000

    module, main, randomizer for machine template name

--- modules/elasticsearch/main.tf
+++ modules/elasticsearch/main.tf
@@ -6,3 +6,18 @@
 */
 
 // --- Machine Template --- //
+// TODO - Refactor using
+//      https://github.com/terraform-google-modules/terraform-google-vm
+resource "random_string" "random" {
+  length = 8
+  lower = true
+  upper = false
+  special = false
+  keepers = {
+    elastic_search_template_machine_type = local.elastic_search_template_machine_type,
+    elastic_search_template_source_image = local.elastic_search_template_source_image,
+    elastic_search_template_tags = join("", sort(local.elastic_search_template_tags)),
+    vm_elastic_search_version = var.vm_elastic_search_version
+  }
+}
+
