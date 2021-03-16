ef17b54a858b5eb9f0492a8dbb5b6e4f3f068ef6
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 12:30:18 +0000

    module firewall configuration, allow traffic to clickhouse nodes

--- modules/clickhouse/firewall.tf
+++ modules/clickhouse/firewall.tf
@@ -1,1 +1,15 @@
 // --- Module Firewall Configuration --- //
+// Clickhouse Firewall Rules --- //
+resource "google_compute_firewall" "vpc_netfw_clickhouse_node" {
+  name = "${var.network_name}-${var.deployment_region}-allow-clickhouse-node"
+  description = "Firewall rule for allowing Clickhouse Requests Traffic (HTTP and CLI)"
+  network = var.network_self_link
+
+  allow {
+      protocol = "tcp"
+      ports = [ local.clickhouse_http_req_port, local.clickhouse_cli_req_port ]
+  }
+
+  target_tags = [ local.fw_tag_clickhouse_node ]
+  source_ranges = var.network_source_ranges
+}
