dd23bf2dcf4766b0a28128432ebf4458258b98e2
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 05:15:37 +0000

    custom VPC definition based on the computed network map for the given deployment regions

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+// --- Networking --- //
+// Custom VPC --- //
+module "vpc_network" {
+  source  = "terraform-google-modules/network/google"
+  version = "~> 3.0"
+
+  project_id = var.config_project_id
+  network_name = local.vpc_network_name
+  routing_mode = "GLOBAL"
+  auto_create_subnetworks = false
+
+  subnets = [
+    for region in var.config_deployment_regions: local.vpc_network_region_subnet_map[region]
+  ]
+}
