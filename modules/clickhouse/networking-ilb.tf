1f10c6bf12fd436722f68e264551b8c6dc2fb19f
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 12:42:24 +0000

    module ILB, backend service

--- modules/clickhouse/networking-ilb.tf
+++ modules/clickhouse/networking-ilb.tf
@@ -18,3 +18,24 @@ resource "google_compute_forwarding_rule" "ilb_forwarding_rule" {
     ]
 }
 
+// Backend Service
+resource "google_compute_region_backend_service" "ilb_backend_service" {
+  name = "${var.module_wide_prefix_scope}-ilb-backend-service"
+  region = var.deployment_region
+  load_balancing_scheme = "INTERNAL"
+  depends_on = [
+      google_compute_region_instance_group_manager.regmig_clickhouse
+    ]
+
+  backend {
+    group = google_compute_region_instance_group_manager.regmig_clickhouse.instance_group
+    //balancing_mode = "UTILIZATION"
+    //capacity_scaler = 1.0
+  }
+
+  protocol = "TCP"
+  timeout_sec = 10
+
+  health_checks = [ google_compute_region_health_check.ilb_backend_healthcheck.id ]
+}
+
