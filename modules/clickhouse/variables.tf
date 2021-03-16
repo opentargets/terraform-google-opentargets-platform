6d3de1575f75430d38ff4cadeb1c25974f23b04e
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 12:13:13 +0000

    module clickhouse, input parameters, general deployment information, scoping name

--- modules/clickhouse/variables.tf
+++ modules/clickhouse/variables.tf
@@ -1,2 +1,7 @@
 // --- Module Input Parameters --- //
 // General deployment input parameters --- //
+variable "module_wide_prefix_scope" {
+  description = "The prefix provided here will scope names for those resources created by this module, default 'otpdevch'"
+  default = "otpdevch"
+}
+
