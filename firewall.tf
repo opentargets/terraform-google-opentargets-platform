40ae120beb6466f329c548ff2ddda9936fc6114a
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-16 05:22:53 +0000

    firewall rules, allow http traffic

--- firewall.tf
+++ firewall.tf
@@ -13,3 +13,18 @@ resource "google_compute_firewall" "vpc_netfw_allow_icmp" {
   }
 }
 
+// Allow HTTP traffic to tagged nodes --- //
+resource "google_compute_firewall" "vpc_netfw_allow_http" {
+  name = "${module.vpc_network.network_name}-allow-http"
+  description = "VPC-wide firewall configuration to allow HTTP on VMs tagged accrodingly"
+  network = module.vpc_network.network_self_link
+  depends_on = [ module.vpc_network ]
+
+  allow {
+      protocol = "tcp"
+      ports = [ "80", "8080" ]
+  }
+
+  target_tags = [ local.fw_tag_http ]
+}
+
