bedcfed8b49782cf029954213db948d9d97b4643
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-17 13:38:20 +0000

    api module, input parameters, name scoping prefix

--- modules/api/variables.tf
+++ modules/api/variables.tf
@@ -1,2 +1,7 @@
 // --- Module Input Parameters --- //
 // General deployment input parameters --- //
+variable "module_wide_prefix_scope" {
+  description = "Scoping prefix for resources names deployed by this module, default 'otpdevapi'"
+  default = "otpdevapi"
+}
+
