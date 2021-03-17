ed347ffe4f47dc656c2bc792f1ddcd6fa3242d8b
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-17 13:39:29 +0000

    api module, input parameters, networking configuration for deployment

--- modules/api/variables.tf
+++ modules/api/variables.tf
@@ -9,3 +9,28 @@ variable "project_id" {
   description = "Project ID where to deploy resources"
 }
 
+variable "network_name" {
+  description = "Name of the network where resources should be connected to, default 'default'"
+  default = "default"
+}
+
+variable "network_self_link" {
+  description = "Self link to the network where resources should be connected when deployed"
+  default = "default"
+}
+
+variable "network_subnet_name" {
+  description = "Name of the subnet, within the 'network_name', and the given region, where instances should be connected to"
+  default = "main-subnet"
+}
+
+variable "network_source_ranges_map" {
+  description = "CIDR that represents which IPs we want to grant access to the deployed resources"
+/*[
+    region = {
+      subnet_ip = "CIDR"
+    }
+  ]
+ */
+}
+
