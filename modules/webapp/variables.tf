fcd8aca5b969dc8272d6120858e0a98f3dc7224c
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-18 12:52:27 +0000

    web app module, input parameters, general deployment information

--- modules/webapp/variables.tf
+++ modules/webapp/variables.tf
@@ -1,1 +1,16 @@
 // --- Web Application Module Input Parameters --- //
+// General Deployment Information --- //
+variable "module_wide_prefix_scope" {
+  description = "Scoping prefix for naming resources in this deployment, default 'otpdevwebapp'"
+  default = "otpdevwebapp"
+}
+
+variable "project_id" {
+  description = "Project ID where resources will be deployed"
+}
+
+variable "location" {
+  description = "This input value sets the bucket's location. Multi-Region or Regional buckets location values are supported, see https://cloud.google.com/storage/docs/locations#location-mr for more information. By default, the bucket is regional, location 'EUROPE-WEST4'"
+  default = "EUROPE-WEST4"
+}
+
