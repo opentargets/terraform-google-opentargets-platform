8b215b24da9b7c0747b0623b862f784a46f49257
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-18 10:39:55 +0000

    api main, module description

---
+++ modules/api/main.tf
@@ -0,0 +1,7 @@
+// Open Targets Platform API deployment definition
+// Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+
+/*
+    This module defines a multi-regional deployment of Open Target Platform API
+*/
+
