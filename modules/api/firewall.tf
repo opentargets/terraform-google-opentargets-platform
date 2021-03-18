0b6bd0c5f6b21a67f9214be88e712602138764aa
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-18 07:04:47 +0000

    api firewall rules

---
+++ modules/api/firewall.tf
@@ -0,0 +1,19 @@
+// --- API Firewall definition --- //
+// Allow traffic to API --- //
+resource "google_compute_firewall" "vpc_netfw_otpapi_node" {
+  count = length(var.deployment_regions)
+
+  name = "${var.network_name}-${var.deployment_regions[count.index]}-allow-otpapi-node"
+  description = "Firewall rule to allow API inbound traffic"
+  network = var.network_self_link
+
+  allow {
+      protocol = "tcp"
+      ports = [ local.otp_api_port ]
+  }
+
+  target_tags = [ local.fw_tag_otp_api_node ]
+  source_ranges = [
+    var.network_source_ranges_map[var.deployment_regions[count.index]].source_range
+  ]
+}
