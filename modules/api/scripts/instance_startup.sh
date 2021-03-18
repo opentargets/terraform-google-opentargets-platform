99b9b0dfbb71e0e37998a5e0f472fade1ae1d060
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-18 07:06:01 +0000

    API VM instance startup script

---
+++ modules/api/scripts/instance_startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "[LAUNCH] Open Targets Platform API"
+docker run -d \
+  -p ${OTP_API_PORT}:${OTP_API_PORT} \
+  -e SLICK_CLICKHOUSE_URL='${SLICK_CLICKHOUSE_URL}' \
+  -e ELASTICSEARCH_HOST='${ELASTICSEARCH_HOST}' \
+  quay.io/opentargets/platform-api-beta:${PLATFORM_API_VERSION}
