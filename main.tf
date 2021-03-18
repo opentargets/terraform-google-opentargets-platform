841a463a64589d78b41b4569976ec2a3cd881867
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-18 11:29:51 +0000

    wire in api module

--- main.tf
+++ main.tf
@@ -61,3 +61,53 @@ module "backend_clickhouse" {
   deployment_region = var.config_deployment_regions[count.index]
   deployment_target_size = 1
 }
+
+// --- API --- //
+module "backend_api" {
+  source = "./modules/api"
+  project_id = var.config_project_id
+  depends_on = [
+    module.vpc_network,
+    module.backend_elastic_search,
+    module.backend_clickhouse
+  ]
+  module_wide_prefix_scope = "${var.config_release_name}-api"
+  network_name = module.vpc_network.network_name
+  network_self_link = module.vpc_network.network_self_link
+  network_subnet_name = local.vpc_network_main_subnet_name
+  network_source_ranges_map = zipmap(
+    var.config_deployment_regions,
+    [ 
+      for region in var.config_deployment_regions: {
+        source_range = local.vpc_network_region_subnet_map[region].subnet_ip
+      } 
+    ]
+  )
+  // We are using an root module defined GLB, so we need this tag to be appended to api nodes, for them to be visible to the GLB
+  vm_firewall_tags = [ local.tag_glb_target_node ]
+  // API VMs configuration
+  vm_platform_api_image_version = "0.55.8"
+  vm_api_vcpus = "2"
+  vm_api_mem = "7680"
+  vm_api_image = "cos-stable"
+  vm_api_image_project = "cos-cloud"
+  vm_api_boot_disk_size = "10GB"
+  backend_connection_map = zipmap(
+    var.config_deployment_regions,
+    [
+      for idx, region in var.config_deployment_regions: {
+        host_clickhouse = module.backend_clickhouse[idx].ilb_ip_address
+        host_elastic_search = module.backend_elastic_search[idx].ilb_ip_address
+      }
+    ]
+  )
+  deployment_regions = var.config_deployment_regions
+  deployment_target_size = 1
+  // This can be
+  //  INTERNAL   - ILB
+  //  GLOBAL    - GLB
+  //  NONE      - To not attach a load balancer to the instance groups
+  load_balancer_type = "NONE"
+  // I have to pass this value until I implement a validation mechanism, but the module won't use it, because it's set to 'NONE' LB
+  dns_domain_api = local.dns_platform_api_dns_name
+}
