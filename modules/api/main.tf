a4ca707f4be5fcbdfbd81553619d066cd03f571a
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-18 10:40:57 +0000

    api main, randomize instance template name2

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -5,3 +5,19 @@
     This module defines a multi-regional deployment of Open Target Platform API
 */
 
+// --- Machine Template --- //
+// TODO - Refactor using
+//      https://github.com/terraform-google-modules/terraform-google-vm
+resource "random_string" "random" {
+  length = 8
+  lower = true
+  upper = false
+  special = false
+  keepers = {
+    otpapi_template_tags = join("", sort(local.otpapi_template_tags)),
+    otpapi_template_machine_type = local.otpapi_template_machine_type,
+    otpapi_template_source_image = local.otpapi_template_source_image,
+    vm_platform_api_image_version = var.vm_platform_api_image_version
+  }
+}
+
