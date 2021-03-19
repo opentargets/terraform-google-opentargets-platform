068369242fdda31fee90792075f91c6bae2e4172
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-19 09:57:10 +0000

    platform GLB

---
+++ networking-glb.tf
@@ -0,0 +1,169 @@
+// --- Platform Global Load Balancer --- //
+// This file defines a Global Load Balancer for the platform (L7 with SSL Termination)
+
+// --- Web App backend --- //
+resource "google_compute_backend_bucket" "webapp" {
+  depends_on = [
+    module.web_app
+  ]
+
+  name = "${var.config_release_name}-backend-webapp"
+  description = "Bucket backend for the web application"
+  bucket_name = module.web_app.bucket.website_bucket_name
+  enable_cdn = true
+}
+
+// --- Platform Global Load Balancer --- //
+// URL Map
+resource "google_compute_url_map" "url_map_platform_glb" {
+  name = "${var.config_release_name}-glb-platform"
+  // Web frontend as default service
+  default_service = google_compute_backend_bucket.webapp.self_link
+
+  // Hosts - Web Application ---
+  host_rule {
+    hosts = local.glb_dns_platform_webapp_domain_names
+    path_matcher = "webapp-paths"
+  }
+  // Hosts - Platform API ---
+  host_rule {
+    hosts = local.glb_dns_platform_api_dns_names
+    path_matcher = "api-paths"
+  }
+  // Path Matchers --- //
+  // Paths - Web Application ---
+  path_matcher {
+    name = "webapp-paths"
+    default_service = google_compute_backend_bucket.webapp.self_link
+  }
+  // Paths - Platform API ---
+  path_matcher {
+    name = "api-paths"
+    default_service = module.glb_platform.backend_services["platformapi"].self_link
+  }
+}
+
+module "glb_platform" {
+  source            = "GoogleCloudPlatform/lb-http/google"
+  version           = "~> 4.4"
+
+  // Dependencies
+  depends_on = [ 
+    module.vpc_network,
+    google_compute_backend_bucket.webapp, 
+    module.backend_api
+  ]
+
+  project           = var.config_project_id
+  name              = "${var.config_release_name}-glb"
+  target_tags       = [ local.tag_glb_target_node ]
+  firewall_networks = [ module.vpc_network.network_name ]
+
+  // Custom URL Map
+  create_url_map = false
+  url_map = google_compute_url_map.url_map_platform_glb.self_link
+
+  // SSL Configuration
+  //ssl = true
+  //managed_ssl_certificate_domains = concat(local.dns_platform_webapp_domain_names, [ local.dns_platform_api_dns_name ])
+  //use_ssl_certificates = false
+  //https_redirect = true
+  
+  backends = {
+    // Web application is the default backend
+    default = {
+      description                     = "The Web Application is the default backend"
+      protocol                        = "HTTP"
+      port                            = 80
+      port_name                       = "http"
+      timeout_sec                     = 10
+      enable_cdn                      = true
+      custom_request_headers          = null
+      security_policy                 = null
+
+      connection_draining_timeout_sec = null
+      session_affinity                = null
+      affinity_cookie_ttl_sec         = null
+
+      health_check = {
+        check_interval_sec  = null
+        timeout_sec         = null
+        healthy_threshold   = null
+        unhealthy_threshold = null
+        request_path        = "/"
+        port                = 80
+        host                = null
+        logging             = null
+      }
+
+      log_config = {
+        enable = true
+        sample_rate = 1.0
+      }
+
+      groups = [
+        // No instance groups here, as the default backend will be the web application
+      ]
+
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = null
+        oauth2_client_secret = null
+      }
+    }
+    // Platform API
+    platformapi = {
+      description                     = "Backend configuration for Open Targets Platform API"
+      protocol                        = "HTTP"
+      port                            = module.backend_api.api_port
+      port_name                       = module.backend_api.api_port_name
+      timeout_sec                     = 10
+      enable_cdn                      = false
+      custom_request_headers          = null
+      security_policy                 = null
+
+      connection_draining_timeout_sec = null
+      session_affinity                = null
+      affinity_cookie_ttl_sec         = null
+
+      health_check = {
+        check_interval_sec  = null
+        timeout_sec         = null
+        healthy_threshold   = null
+        unhealthy_threshold = null
+        request_path        = "/_ah/health"
+        port                = module.backend_api.api_port
+        host                = null
+        logging             = null
+      }
+
+      log_config = {
+        enable = true
+        sample_rate = 1.0
+      }
+
+      // Connect all the API instance groups
+      groups = [
+        for region, regmig in module.backend_api.map_region_to_instance_group_manager : {
+            group                        = regmig.instance_group
+            balancing_mode               = null
+            capacity_scaler              = null
+            description                  = null
+            max_connections              = null
+            max_connections_per_instance = null
+            max_connections_per_endpoint = null
+            max_rate                     = null
+            max_rate_per_instance        = null
+            max_rate_per_endpoint        = null
+            max_utilization              = null
+        }
+      ]
+
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = null
+        oauth2_client_secret = null
+      }
+    }
+  }
+}
