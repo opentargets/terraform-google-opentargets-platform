98bca58f49e781906f49b61f4dc218090997db0e
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-19 13:31:16 +0000

    development inspection VM definition

---
+++ dev-canary.tf
@@ -0,0 +1,24 @@
+// --- This file contains resources that will be activated when development mode is 'ON', for debugging purposes --- //
+// Canary for the infrastructure mine --- //
+resource "google_compute_instance" "gce_testvm" {
+  // This definition will deploy a small VM in each deployment region for debugging communication and other infrastructure issues
+  count = length(var.config_deployment_regions) * local.dev_mode_conditional_deployment
+
+  name = "mbdev-vm-test-${count.index}"
+  machine_type = "e2-small"
+  zone = "${var.config_deployment_regions[count.index]}-b"
+  depends_on = [ module.vpc_network ]
+
+  boot_disk {
+    initialize_params {
+        image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = module.vpc_network.network_self_link
+    subnetwork = "main-subnet"
+  }
+
+  tags = [ "ssh" ]
+}
