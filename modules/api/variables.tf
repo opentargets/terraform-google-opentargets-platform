69d1897debb98530fe6197f88159e8f01f78ac29
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-19 12:45:09 +0000

    api module input parameters type information

--- modules/api/variables.tf
+++ modules/api/variables.tf
@@ -2,30 +2,37 @@
 // General deployment input parameters --- //
 variable "module_wide_prefix_scope" {
   description = "Scoping prefix for resources names deployed by this module, default 'otpdevapi'"
+  type = string
   default = "otpdevapi"
 }
 
 variable "project_id" {
   description = "Project ID where to deploy resources"
+  type = string
 }
 
 variable "network_name" {
   description = "Name of the network where resources should be connected to, default 'default'"
+  type = string
   default = "default"
 }
 
 variable "network_self_link" {
   description = "Self link to the network where resources should be connected when deployed"
+  type = string
   default = "default"
 }
 
 variable "network_subnet_name" {
   description = "Name of the subnet, within the 'network_name', and the given region, where instances should be connected to"
+  type = string
   default = "main-subnet"
 }
 
 variable "network_source_ranges_map" {
   description = "CIDR that represents which IPs we want to grant access to the deployed resources"
+  // This iteration won't check the type, this definition will be refined in future iterations
+  type = any
 /*[
     region = {
       subnet_ip = "CIDR"
@@ -37,50 +44,61 @@ variable "network_source_ranges_map" {
 // --- API Instances configuration --- //
 variable "deployment_regions" {
   description = "List of regions where the API nodes should be deployed"
+  type = list(string)
 }
 
 variable "vm_firewall_tags" {
   description = "List of additional tags to attach to API nodes"
+  type = list(string)
   default = [ ]
 }
 
 variable "vm_platform_api_image_version" {
   description = "API Docker image version to use in deployment"
+  type = string
 }
 
 variable "vm_api_vcpus" {
   description = "CPU count for API nodes, default '2'"
+  type = number
   default = "2"
 }
 
 variable "vm_api_mem" {
   description = "Amount of memory allocated for API nodes (MiB), default '7680'"
+  type = number
   default = "7680"
 }
 
 variable "vm_api_image" {
   description = "VM image to use for API nodes, default 'cos-stable'"
+  type = string
   default = "cos-stable"
 }
 
 variable "vm_api_image_project" {
   description = "Project hosting the VM image, default 'cos-cloud'"
+  type = string
   default = "cos-cloud"
 }
 
 variable "vm_api_boot_disk_size" {
   description = "Boot disk size for API nodes, default '10GB'"
+  type = string
   default = "10GB"
 }
 
 variable "deployment_target_size" {
   description = "Initial API node count per region"
+  type = number
   default = 1
 }
 
 // --- Backend Connection Information --- //
 variable "backend_connection_map" {
   description = "Information on where to connect to data backend services"
+  // This iteration won't check the type, this definition will be refined in future iterations
+  type = any
   /*
   {
     "region" = {
@@ -93,8 +111,8 @@ variable "backend_connection_map" {
 
 // --- Load Balancer configuration --- //
 variable "load_balancer_type" {
-  type = string
   description = "This will tell the module whether an ILB, GLB or no load balancer at all should be created"
+  type = string
   validation {
     condition = contains([ "INTERNAL", "GLOBAL", "NONE" ], var.load_balancer_type)
     error_message = "Allowed values for 'load_balancer_type' are [ 'INTERNAL', 'GLOBAL', 'NONE' ]."
@@ -104,4 +122,5 @@ variable "load_balancer_type" {
 // --- DNS configuration --- //
 variable "dns_domain_api" {
   description = "This is the baseline DNS to use for all the forwarding rules that will be configured in the GLB, if chosen"
+  type = string
 }
