2c29b45b19e10f699d7aca01bbbf9e1d619839e8
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-19 17:27:33 +0000

    health checks firewall rules, module elastic search

--- modules/clickhouse/firewall.tf
+++ modules/clickhouse/firewall.tf
@@ -13,3 +13,18 @@ resource "google_compute_firewall" "vpc_netfw_clickhouse_node" {
   target_tags = [ local.fw_tag_clickhouse_node ]
   source_ranges = var.network_source_ranges
 }
+
+// Health Checks Traffic --- //
+resource "google_compute_firewall" "vpc_netfw_clickhouse_healthchecks" {
+  name = "${var.network_name}-${var.deployment_region}-allow-clickhouse-healthchecks"
+  description = "Firewall rule for allowing Clickhouse nodes Health Checks"
+  network = var.network_self_link
+
+  allow {
+      protocol = "tcp"
+      ports = [ local.clickhouse_http_req_port ]
+  }
+
+  target_tags = [ local.fw_tag_clickhouse_node ]
+  source_ranges = concat(var.network_source_ranges, var.network_sources_health_checks)
+}
