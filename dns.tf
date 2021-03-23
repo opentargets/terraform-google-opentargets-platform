1dbb30aa5cb8c3e64cb70187b646c738d781836b
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-23 10:33:54 +0000

    DNS A record TTL to 5 seconds

--- dns.tf
+++ dns.tf
@@ -4,7 +4,7 @@ resource "google_dns_record_set" "dns_a_api_glb" {
   // Common
   managed_zone = var.config_dns_managed_zone_name
   type = "A"
-  ttl = 300
+  ttl = 5
 
   // Entry
   name = local.dns_platform_api_dns_name
@@ -17,7 +17,7 @@ resource "google_dns_record_set" "dns_a_webapp_glb" {
     // Common
     managed_zone = var.config_dns_managed_zone_name
     type = "A"
-    ttl = 300
+    ttl = 5
     // Entry
     name = local.dns_platform_webapp_domain_names[count.index]
     rrdatas = [ module.glb_platform.external_ip ]
