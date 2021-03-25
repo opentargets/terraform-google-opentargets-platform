16c0467e04f0dab17e30d17edf695b643cbfc0c7
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-03-25 14:24:50 +0000

    Helper for variable replacement in file

---
+++ helpers/replace_with_env_vars_values.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Given an environment file that defines values for keys, replace them in the destination file
+
+# Input
+envfile=$1
+dstfile=$2
+
+echo "[TERRAFORM] Loading environment from '${envfile}'"
+for key in $( cat ${envfile} | awk '{$1=$1};1' | egrep -o "TF_VAR_[_A-Za-z]+" ) ; do
+    source ${envfile} ; echo -e "\t[TERRAFORM] Injecting '${key}=${!key}'" ;
+    source ${envfile} ; sed -E -i ".bak" "s/${key}(\W|$)/\"${!key}\"/g" ${dstfile} ;
+done
