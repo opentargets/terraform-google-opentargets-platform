7e49a52fc2116ae1f417ac6ee83d2a668e233f31
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-04-12 06:02:10 +0100

    software release cycle helper script

---
+++ release.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# This script is a software release cycle helper
+#
+# Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+
+# Defaults
+verb='nothing'
+message=""
+version=$(cat VERSION)
+
+if [ $# -lt 1 ]
+then
+  echo "usage: $(basename $0) patch|minor|major <'descriptive message for this release'>"
+  exit 1
+fi
+
+# Read verb
+verb=$1
+# Read the possible message
+if [ $# -gt 1 ]
+then
+    message=$2
+fi
+# Everything ok?
+ok=false
+
+# Process release
+if [ "${verb}" == "patch" ]
+then
+    echo "<===|DEVOPS|===> [RELEASE] PATCH"
+    echo -e "\tCurrent version '${version}'"
+    version=$(./increment_version.sh -p ${version})
+    ok=true
+fi
+if [ "${verb}" == "minor" ]
+then
+    echo "<===|DEVOPS|===> [RELEASE] MINOR"
+    echo -e "\tCurrent version '${version}'"
+    version=$(./increment_version.sh -m ${version})
+    ok=true
+fi
+if [ "${verb}" == "major" ]
+then
+    echo "<===|DEVOPS|===> [RELEASE] MAJOR"
+    echo -e "\tCurrent version '${version}'"
+    version=$(./increment_version.sh -M ${version})
+    ok=true
+fi
+
+# Should we publish the changes?
+if $ok ; then
+    # TODO - There is room for detecting if anything went wrong here and revert the changes (feature request)
+    echo -e "\tNew version '${version}'"
+    echo "${version}" > VERSION
+    echo -e "\tCommit, push and tag version"
+    if [ "${message}" != "" ] ; then
+        echo -e "\tVersion Tag message: ${message}"
+    else
+        echo -e "\tNO VERSION tag message included"
+        message="New release ${version}"
+    fi
+    git commit -am "${message}"
+    # I need this commit to make sure the latest one is in sync with master branch
+    git push
+    git tag ${version} -m "${message}"
+    git push origin ${version}
+else
+    echo -e "\t--- ABORT --- Something went wrong"
+fi
