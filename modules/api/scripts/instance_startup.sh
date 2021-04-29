3d6a97b52369882d6c194f3084bf8780436847c8
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-04-29 11:26:07 +0100

    use gcp logs in docker

--- modules/api/scripts/instance_startup.sh
+++ modules/api/scripts/instance_startup.sh
@@ -2,6 +2,7 @@
 
 echo "[LAUNCH] Open Targets Platform API"
 docker run -d \
+  --log-driver=gcplogs \
   -p ${OTP_API_PORT}:${OTP_API_PORT} \
   -e SLICK_CLICKHOUSE_URL='${SLICK_CLICKHOUSE_URL}' \
   -e ELASTICSEARCH_HOST='${ELASTICSEARCH_HOST}' \
