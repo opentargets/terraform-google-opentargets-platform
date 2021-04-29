cbb793c71165ee929d60fa8f30c9ca136278105b
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-04-29 11:38:33 +0100

    revert change to template

--- modules/api/scripts/instance_startup.sh
+++ modules/api/scripts/instance_startup.sh
@@ -2,7 +2,6 @@
 
 echo "[LAUNCH] Open Targets Platform API"
 docker run -d \
-  --log-driver=gcplogs \
   -p ${OTP_API_PORT}:${OTP_API_PORT} \
   -e SLICK_CLICKHOUSE_URL='${SLICK_CLICKHOUSE_URL}' \
   -e ELASTICSEARCH_HOST='${ELASTICSEARCH_HOST}' \
