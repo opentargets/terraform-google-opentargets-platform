d128f82c27dd5fa0a0d9dfb982a5fc0e0386e8e9
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-04-29 16:36:57 +0100

    module webapp, webservers compute, compute zones

---
+++ modules/webapp/webservers-compute.tf
@@ -0,0 +1,23 @@
+// Definition of active web servers for the web application
+resource "random_string" "random_web_server_suffix" {
+  length = 8
+  lower = true
+  upper = false
+  special = false
+  keepers = {
+    webapp_bucket_name = local.bucket_name
+    // TODO - nginx version
+  }
+}
+
+// TODO - Access to Available compute zones in the given region --- //
+data "google_compute_zones" "available" {
+  count = length(var.webserver_deployment_regions)
+  
+  region = var.webserver_deployment_regions[count.index]
+}
+// TODO - Service Account --- //
+// TODO - Instance Template --- //
+// TODO - Helath Check --- //
+// TODO - RegMIG --- //
+// TODO - Autoscalers --- //
