87aab4ae2c827d0de7d2d5d3106fd89358c464cd
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-04-30 11:20:32 +0100

    web server firewall rules

---
+++ modules/webapp/webservers-firewall.tf
@@ -0,0 +1,39 @@
+// --- API Firewall definition --- //
+// Allow traffic to API --- //
+resource "google_compute_firewall" "vpc_netfw_webserver_node" {
+  count = length(var.webserver_deployment_regions)
+
+  name = "${var.network_name}-${var.webserver_deployment_regions[count.index]}-allow-webserver-node"
+  description = "Firewall rule to allow Web Server inbound traffic"
+  network = var.network_self_link
+
+  allow {
+      protocol = "tcp"
+      ports = [ local.webapp_webserver_port ]
+  }
+
+  target_tags = [ local.fw_tag_webserver_node ]
+  source_ranges = [
+    var.network_source_ranges_map[var.webserver_deployment_regions[count.index]].source_range
+  ]
+}
+
+// Health Checks Traffic --- //
+resource "google_compute_firewall" "vpc_netfw_webserver_healthchecks" {
+  count = length(var.webserver_deployment_regions)
+
+  name = "${var.network_name}-${var.webserver_deployment_regions[count.index]}-allow-webserver-healthchecks"
+  description = "Firewall rule to allow Web Server Health Checks traffic"
+  network = var.network_self_link
+
+  allow {
+      protocol = "tcp"
+      ports = [ local.webapp_webserver_port ]
+  }
+
+  target_tags = [ local.fw_tag_webserver_node ]
+  source_ranges = concat(
+    [ var.network_source_ranges_map[var.webserver_deployment_regions[count.index]].source_range ],
+    var.network_sources_health_checks
+  )
+}
