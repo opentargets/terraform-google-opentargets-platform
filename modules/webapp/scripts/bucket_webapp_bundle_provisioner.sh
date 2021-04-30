8ec945af856bd613e3889f32a9f200be5dee75a7
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-04-30 11:04:06 +0100

    updated webapp bundle provisioner to create the deployment bundle tarball

--- modules/webapp/scripts/bucket_webapp_bundle_provisioner.sh
+++ modules/webapp/scripts/bucket_webapp_bundle_provisioner.sh
@@ -30,6 +30,9 @@ gsutil cp -r ${data_context_url}/* ${data_context_dst_folder}/.
 echo "[CLEAN] Remove context template"
 rm -f ${file_name_devops_context_template}
 rm -f "${file_name_devops_context_instance}.bak"
+echo "[PACKAGING] Preparing Web Application deployment bundle"
+tar czvf ../${deployment_bundle_filename} *
+cp ../${deployment_bundle_filename} .
 echo "[DEPLOY] Uploading webapp to bucket '${bucket_webapp_url}'"
 gsutil cp -r `pwd`/* ${bucket_webapp_url}
 echo "[DONE] Process Completed ---"
