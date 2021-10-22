06c886ec6297df2c54d70d21b591a71a34de0115
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-10-22 12:06:01 +0100

    add logging driver option to docker

--- modules/api/scripts/instance_startup.sh
+++ modules/api/scripts/instance_startup.sh
@@ -3,6 +3,7 @@
 echo "[LAUNCH] Open Targets Platform API"
 docker run -d \
   -p ${OTP_API_PORT}:${OTP_API_PORT} \
+  --log-driver=gcplogs \
   -e SLICK_CLICKHOUSE_URL='${SLICK_CLICKHOUSE_URL}' \
   -e ELASTICSEARCH_HOST='${ELASTICSEARCH_HOST}' \
   quay.io/opentargets/platform-api-beta:${PLATFORM_API_VERSION}
