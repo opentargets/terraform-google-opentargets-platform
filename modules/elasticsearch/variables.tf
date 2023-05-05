921ee063e29a60c7736a31f1ea5eef948dfdd7ec
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-05-05 13:33:02 +0100

    elastic search default image for the VM

--- modules/elasticsearch/variables.tf
+++ modules/elasticsearch/variables.tf
@@ -79,17 +79,19 @@ variable "vm_elastic_search_mem" {
 variable "vm_elastic_search_image" {
   description = "VM Image to use for Elastic Search instances"
   type        = string
+  default = "cos-stable"
 }
 
 variable "vm_elastic_search_image_project" {
   description = "Project hosting the Elastic Search VM Instance image"
   type        = string
+  default     = "cos-cloud"
 }
 
 variable "vm_elastic_search_boot_disk_size" {
   description = "Elastic Search instances boot disk size, default '500GB'"
   type        = string
-  default     = "500GB"
+  default     = "16GB"
 }
 
 variable "vm_flag_preemptible" {
