b498a63cfddb827247b569bfaada86cd82e417f6
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-05-05 14:05:10 +0100

    ES startup script with data volume

--- modules/elasticsearch/scripts/instance_startup.sh
+++ modules/elasticsearch/scripts/instance_startup.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
-# Startup script for Elastic Search VM Instance
+# Local environment
+export path_mount_es_data_volume="/mnt/esdata"
+export device_disk_es_data=${GCP_DEVICE_DISK_PREFIX}${DATA_DISK_DEVICE_NAME_ES}
+export path_es_data_volume=${path_mount_es_data_volume}/data
+export docker_volume_name_es="esdata"
+export docker_image_string_es="docker.elastic.co/elasticsearch/elasticsearch-oss:${ELASTIC_SEARCH_VERSION}"
+export es_docker_container_name="otp-es"
+export es_cluster_name=`hostname`
+export es_vol_path_data=${path_es_data_volume}
 
-echo "---> [LAUNCH] Open Targets Platform Elastic Search"
-# Get machine available memory
-export MACHINE_SIZE=`cat /proc/meminfo | grep MemTotal | grep -o '[0-9]\+'`
-# Set half the available RAM for the JVM
-export JVM_SIZE=`expr $MACHINE_SIZE / 2097152`
-# Make sure the image is not in use
-docker stop elasticsearch
-docker rm elasticsearch
+# Logging functions
+function log() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]$@"
+}
+function Logi() {
+  log "[INFO] $@"
+}
+
+function Logw() {
+  log "[WARNING] $@"
+}
+
+function Loge() {
+  log "[ERROR] $@"
+}
+
+# Prepare the data volume
+logi "Prepare data mount point at '$${path_mount_es_data_volume}'"
+mkdir -p $${path_mount_es_data_volume}
+logi "Mount Elastic Seardh data disk device '$${device_disk_es_data}' at '$${path_mount_es_data_volume}'"
+mount $${device_disk_es_data} $${path_mount_es_data_volume}
+logi "Create a Docker volume at the Elastic Search Data Volume '$${docker_volume_name_es}' bound to folder '$${path_es_data_volume}'"
+docker volume create --name $${docker_volume_name_es} --opt type=none --opt device=$${path_es_data_volume} --opt o=bind
 # Launch Elastic Search
-docker run -d \
-  --name elasticsearch \
+logi "Running Elastic Search via Docker, using image $${docker_image_string_es}"
+logi "Setting vm.max_map_count to 262144"
+sysctl -w vm.max_map_count=262144
+logi "[INFO] Elastic Search docker container name: $${es_docker_container_name}, cluster name: $${es_cluster_name}, data volume: $${docker_volume_name_es}"
+docker run --rm -d \
+  --name $${es_docker_container_name} \
   --log-driver=gcplogs \
   -p 9200:9200 \
   -p 9300:9300 \
-  -e discovery.type=single-node \
-  -e bootstrap.memory_lock=true \
-  -e repositories.url.allowed_urls='https://storage.googleapis.com/*,https://*.amazonaws.com/*' \
-  -e thread_pool.write.queue_size=1000 \
-  -e cluster.name=`hostname` \
-  -e network.host=0.0.0.0 \
-  -e search.max_open_scroll_context=5000 \
-  -e ES_JAVA_OPTS="-Xms$${JVM_SIZE}g -Xmx$${JVM_SIZE}g" \
-  -v esdata:/usr/share/elasticsearch/data \
-  -v /var/elasticsearch/cred:/usr/share/elasticsearch/cred \
-  -v /var/elasticsearch/log:/var/log/elasticsearch \
-  docker.elastic.co/elasticsearch/elasticsearch-oss:${ELASTIC_SEARCH_VERSION}
+  -e "path.data=/usr/share/elasticsearch/data" \
+  -e "path.logs=/usr/share/elasticsearch/logs" \
+  -e "cluster.name=$${es_cluster_name}" \
+  -e "network.host=0.0.0.0" \
+  -e "discovery.type=single-node" \
+  -e "discovery.seed_hosts=[]" \
+  -e "bootstrap.memory_lock=true" \
+  -e "search.max_open_scroll_context=5000" \
+  -e "thread_pool.write.queue_size=1000" \
+  -v $${docker_volume_name_es}:/usr/share/elasticsearch/data \
+  --ulimit memlock=-1:-1 \
+  --ulimit nofile=65536:65536 \
+  $${docker_image_string_es}
