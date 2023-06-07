9a568d28e7f93a3d4c9aa99a7b8ccbb6ec2b4577
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-06-07 17:27:08 +0100

    set Xms and Xmx to the same value

--- modules/elasticsearch/scripts/instance_startup.sh
+++ modules/elasticsearch/scripts/instance_startup.sh
@@ -54,7 +54,7 @@ docker run --rm -d \
   -e "discovery.seed_hosts=[]" \
   -e "bootstrap.memory_lock=true" \
   -e "search.max_open_scroll_context=5000" \
-  -e ES_JAVA_OPTS="-Xms1g -Xmx$${JVM_SIZE}g" \
+  -e ES_JAVA_OPTS="-Xms$${JVM_SIZE}g -Xmx$${JVM_SIZE}g" \
   -v $${docker_volume_name_es}:/usr/share/elasticsearch/data \
   --ulimit memlock=-1:-1 \
   --ulimit nofile=65536:65536 \
