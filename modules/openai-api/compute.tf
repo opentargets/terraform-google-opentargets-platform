1fd4924c28ac6173b8a709165a25489b0a70c250
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-06-13 11:13:33 +0100

    compute service account configuration

--- modules/openai-api/compute.tf
+++ modules/openai-api/compute.tf
@@ -12,3 +12,33 @@ resource "random_string" "openai_api_node" {
         startup_script = md5(file("${path.module}/scripts/vm_startup.sh"))
     }
 }
+
+// Access to Available compute zones in the given region --- //
+data "google_compute_zones" "available" {
+  count = length(var.deployment_regions)
+
+  region = var.deployment_regions[count.index]
+}
+
+// --- Service Account Configuration ---
+resource "google_service_account" "gcp_service_acc_openai_api" {
+  project      = var.project_id
+  account_id   = "${var.module_wide_prefix_scope}-svcacc-${random_string.openai_api_node.result}"
+  display_name = "${var.module_wide_prefix_scope}-GCP-service-account"
+}
+
+// Roles ---
+resource "google_project_iam_member" "logging-writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.gcp_service_acc_openai_api.email}"
+}
+
+resource "google_project_iam_member" "monitoring-writer" {
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.gcp_service_acc_openai_api.email}"
+}
+// --- /Service Account Configuration/ ---
+
+// OpenAI API Compute Instance (VM) ---
