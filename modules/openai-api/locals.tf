4ba6c9f24e2bf23fa24c67d50499b303a8edb9f8
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-06-13 11:18:46 +0100

    compute machine type in 'locals'

--- modules/openai-api/locals.tf
+++ modules/openai-api/locals.tf
@@ -1,16 +1,17 @@
 locals {
   // Communication Ports
-    openai_api_port      = 8080
-    openai_api_port_name = "openaiapiport"
-    // Firewall
-    fw_tag_openai_api = "openaiapinode"
-    // Compute
-    fw_vm_tags = concat(
-        var.vm_tags,
-        [local.fw_tag_openai_api]
-    )
-    // Compute Instances (VMs)
-    vm_template_source_image = "${var.vm_image_project}/${var.vm_image}"
-    // Effective Docker image
-    openai_api_docker_image = "${var.openai_api_docker_image}:${var.openai_api_docker_image_version}"
+  openai_api_port      = 8080
+  openai_api_port_name = "openaiapiport"
+  // Firewall
+  fw_tag_openai_api = "openaiapinode"
+  // Compute
+  fw_vm_tags = concat(
+    var.vm_tags,
+    [local.fw_tag_openai_api]
+  )
+  // Compute Instances (VMs)
+  vm_template_source_image = "${var.vm_image_project}/${var.vm_image}"
+  vm_machine_type          = var.vm_machine_type
+  // Effective Docker image
+  openai_api_docker_image = "${var.openai_api_docker_image}:${var.openai_api_docker_image_version}"
 }
