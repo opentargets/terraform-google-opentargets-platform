3d88d3e9ae949d97ea9724edaba5f192ea7e25b9
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-06-15 14:44:04 +0100

    formatting

--- secrets.tf
+++ secrets.tf
@@ -2,20 +2,20 @@
 
 // --- OpenAI API --- //
 resource "google_secret_manager_secret" "openai_api_token" {
-    project = var.config_project_id
+  project = var.config_project_id
 
-    secret_id = "openai-api-token"
-    replication {
-        user_managed {
-            replicas {
-                location = "europe-west1"
-            }
-        }
+  secret_id = "openai-api-token"
+  replication {
+    user_managed {
+      replicas {
+        location = "europe-west1"
+      }
     }
+  }
 }
 
 resource "google_secret_manager_secret_version" "openai_api_token" {
-    secret = google_secret_manager_secret.openai_api_token.id
-    secret_data = "${local.credentials_openai_token}"
+  secret      = google_secret_manager_secret.openai_api_token.id
+  secret_data = local.credentials_openai_token
 }
 
