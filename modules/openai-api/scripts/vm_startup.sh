ad2a688b74c1233947b43ae5438c7464a7361341
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-06-15 11:34:48 +0100

    openai api vm startup script

---
+++ modules/openai-api/scripts/vm_startup.sh
@@ -0,0 +1,8 @@
+echo "[LAUNCH] Open Targets Platform OpenAI API"
+set +x
+docker run -d \
+    -p ${openai_api_external_port}:${openai_api_internal_port} \
+    --name ${openai_api_container_name} \
+    --log-driver=gcplogs \
+    -e OPENAI_TOKEN='${openai_token}' \
+    ${openai_api_docker_image}
