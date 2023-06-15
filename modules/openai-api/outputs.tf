abae8808d53a498c9fdb3ff07300ba771bf5ca9e
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-06-15 11:40:49 +0100

    output information, region to regional instance group managers map

--- modules/openai-api/outputs.tf
+++ modules/openai-api/outputs.tf
@@ -3,3 +3,9 @@ output "deployment_regions" {
   value = var.deployment_regions
 }
 
+output "map_region_to_instance_group_manager" {
+  value = zipmap(
+    var.deployment_regions,
+    google_compute_region_instance_group_manager.remig_openai_api.*
+  )
+}
