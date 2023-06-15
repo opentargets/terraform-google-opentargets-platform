7dd026d4b7468ad4358e616f274ca5f0de13b46c
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-06-15 12:47:26 +0100

    openai api module output values

--- modules/openai-api/outputs.tf
+++ modules/openai-api/outputs.tf
@@ -9,3 +9,13 @@ output "map_region_to_instance_group_manager" {
     google_compute_region_instance_group_manager.remig_openai_api.*
   )
 }
+
+output "openai_api_port" {
+  // Output the listening port for the Open Targets Platform OpenAI API
+  value = local.openai_api_port
+}
+
+output "openai_api_port_name" {
+  // Output the custom named port for the instance group
+  value = local.openai_api_port_name
+}
