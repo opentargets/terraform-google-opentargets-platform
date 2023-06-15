7a90ab3af766f99277c1ce941b41fd73f11fcd9d
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-06-15 15:15:24 +0100

    use the given secret ID for retrieving the token

--- modules/openai-api/scripts/vm_startup.sh
+++ modules/openai-api/scripts/vm_startup.sh
@@ -4,5 +4,5 @@ docker run -d \
     -p ${openai_api_external_port}:${openai_api_internal_port} \
     --name ${openai_api_container_name} \
     --log-driver=gcplogs \
-    -e OPENAI_TOKEN='${openai_token}' \
+    -e OPENAI_TOKEN=$(gcloud secrets versions access latest --secret="${openai_token}") \
     ${openai_api_docker_image}
