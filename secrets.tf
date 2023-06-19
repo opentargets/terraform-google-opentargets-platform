8a51bcf316c6845ea664933dd4d89a988d1fbd06
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-06-19 16:40:51 +0100

    fix, update openai secret ID and trigger updates on dependant modules

--- secrets.tf
+++ secrets.tf
@@ -1,10 +1,21 @@
 // --- Open Targets Platform Operational Secrets --- //
 
+// Random resource for secret ID
+resource "random_string" "openai_token" {
+  length  = 8
+  special = false
+  upper   = false
+  lower   = true
+  keepers = {
+    secret_data = md5(local.credentials_openai_token)
+  }
+}
+
 // --- OpenAI API --- //
 resource "google_secret_manager_secret" "openai_api_token" {
   project = var.config_project_id
 
-  secret_id = "openai-api-token"
+  secret_id = "openai-token-${random_string.openai_token.result}"
   replication {
     user_managed {
       replicas {
