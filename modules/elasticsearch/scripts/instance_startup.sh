f1fa7d5f1b19f3a28533e3d55d22d42f2a2ffaf5
ricardo <remo87@gmail.com>
2025-06-02 13:59:08 +0100

    expose opensearch metrics

--- modules/elasticsearch/scripts/instance_startup.sh
+++ modules/elasticsearch/scripts/instance_startup.sh
@@ -64,7 +64,7 @@ mkdir -p $${path_mount_es_data_volume}
 logi "Mount Elastic Seardh data disk device '$${device_disk_es_data}' at '$${path_mount_es_data_volume}'"
 mount $${device_disk_es_data} $${path_mount_es_data_volume}
 logi "Create a Docker volume at the Elastic Search Data Volume '$${docker_volume_name_es}' bound to folder '$${path_es_data_volume}'"
-docker volume create --name $${docker_volume_name_es} --opt type=none --opt device=$${path_es_data_volume} --opt o=bind
+
 # Launch Elastic Search
 logi "Running Elastic Search via Docker, using image $${docker_image_string_es}"
 logi "Setting vm.max_map_count to 262144"
@@ -76,12 +76,14 @@ export JVM_SIZE=`expr $(expr $MACHINE_SIZE / 1048576) - 1`
 export JVM_SIZE_HALF=`expr $MACHINE_SIZE / 2097152`
 logi "[INFO] Elastic Search docker container name: $${es_docker_container_name}, cluster name: $${es_cluster_name}, data volume: $${docker_volume_name_es}, JVM Memory: $${JVM_SIZE}GiB"
 
+logi "Generating compose file"
 # Create the docker-compose file
 mkdir -p /opt/ot-os
-cat << EOF >> /opt/ot-os/compose.yml
+cd /opt/ot-os
+cat << EOF >> compose.yml
 services:
   opensearch:
-    iamge: $${docker_image_string_es}
+    image: $${docker_image_string_es}
     container_name: $${es_docker_container_name}
     logging:
       driver: gcplogs
@@ -112,7 +114,7 @@ services:
      - '--es.uri=http://$${es_cluster_name}:9200'
     restart: always
     ports:
-    - "127.0.0.1:9114:9114"
+    - "9114:9114"
   node_exporter:
     image: quay.io/prometheus/node-exporter:latest
     container_name: node_exporter
@@ -123,37 +125,24 @@ services:
     restart: unless-stopped
     volumes:
       - '/:/host:ro,rslave'
-
+volumes:
+  $${docker_volume_name_es}:
+    driver_opts:
+      type: "none"
+      device: $${path_es_data_volume}
+      o: bind
 EOF
 
-# docker run --rm -d \
-#   --name $${es_docker_container_name} \
-#   --log-driver=gcplogs \
-#   -p 9200:9200 \
-#   -p 9300:9600 \
-#   -e "path.data=/usr/share/opensearch/data" \
-#   -e "path.logs=/usr/share/opensearch/logs" \
-#   -e "cluster.name=$${es_cluster_name}" \
-#   -e "network.host=0.0.0.0" \
-#   -e "discovery.type=single-node" \
-#   -e "discovery.seed_hosts=[]" \
-#   -e "bootstrap.memory_lock=true" \
-#   -e "search.max_open_scroll_context=5000" \
-#   -e OPENSEARCH_JAVA_OPTS="-Xms$${JVM_SIZE_HALF}g -Xmx$${JVM_SIZE_HALF}g" \
-#   -e DISABLE_SECURITY_PLUGIN=true \
-#   -v $${docker_volume_name_es}:/usr/share/opensearch/data \
-#   --ulimit memlock=-1:-1 \
-#   --ulimit nofile=65536:65536 \
-#   $${docker_image_string_es}
-
+logi "execute docker compose up"
+docker compose up -d
 
 # Wait for Elastic Search to be yellow
 function wait_for_elasticsearch() {
-  log "[INFO] Waiting for Elastic Search to be ready"
+  logi "Waiting for Elastic Search to be ready"
   while ! curl -s http://localhost:9200/_cluster/health?pretty | grep -q '"status" : "yellow"'; do
     sleep 1
   done
-  log "[INFO] Elastic Search is ready"
+  logi "Elastic Search is ready"
 }
 
 wait_for_elasticsearch
