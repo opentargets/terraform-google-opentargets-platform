bdad314994ea526733e0afc6aedc122304741baf
ricardo <remo87@gmail.com>
2025-06-11 16:00:01 +0100

    configure datasource

--- modules/prometheus/main.tf
+++ modules/prometheus/main.tf
@@ -18,7 +18,7 @@ resource "random_string" "random" {
     otpprometheus_template_machine_type = local.otpprometheus_template_machine_type,
     vm_startup_script                   = md5(file("${path.module}/scripts/instance_startup.sh")),
     vm_compose                          = md5(file("${path.module}/config/compose.yml")),
-    vm_prometheus                       = md5(file("${path.module}/config/prometheus.yml")),
+    datasources                         = md5(file("${path.module}/config/datasources.yml")),
     vm_flag_preemptible                 = var.vm_flag_preemptible
   }
 }
