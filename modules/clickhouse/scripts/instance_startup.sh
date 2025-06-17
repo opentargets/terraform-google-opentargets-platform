de98aa826d0c3c872ee39dbe573049bd5d03c272
ricardo <remo87@gmail.com>
2025-06-17 14:37:50 +0100

    refactor compose

--- modules/clickhouse/scripts/instance_startup.sh
+++ modules/clickhouse/scripts/instance_startup.sh
@@ -33,7 +33,7 @@ apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Local environment
-export path_mount_ch_data_volume="/mnt/disks/chdata"
+export path_mount_ch_data_volume=${CH_DATA_VOLUME}
 export device_disk_ch_data=${GCP_DEVICE_DISK_PREFIX}${DATA_DISK_DEVICE_NAME_CH}
 
 # Logging functions
@@ -58,43 +58,11 @@ mkdir -p $${path_mount_ch_data_volume}
 logi "Mount Clickhouse data disk device '$${device_disk_ch_data}' at '$${path_mount_ch_data_volume}'"
 mount $${device_disk_ch_data} $${path_mount_ch_data_volume}
 
-# Launch Clickhouse
-logi "Launching Clickhose via docker image '${DOCKER_IMAGE_CLICKHOUSE}'"
-
 logi "Generating compose file"
 # Create the docker-compose file
 mkdir -p /opt/ot-ch
 cd /opt/ot-ch
-cat << EOF >> compose.yml
-services:
-  clickhouse:
-    image: ${DOCKER_IMAGE_CLICKHOUSE}
-    container_name: $${otp-ch}
-    logging:
-      driver: gcplogs
-    ports:
-      - "9000:9000"
-      - "8123:8123"
-      - "9363:9363"
-    volumes:
-      - $${path_mount_ch_data_volume}/config.d:/etc/clickhouse-server/config.d
-      - $${path_mount_ch_data_volume}/users.d:/etc/clickhouse-server/users.d
-      - $${path_mount_ch_data_volume}/data:/var/lib/clickhouse
-    ulimits:
-      nofile:
-        soft: 262144
-        hard: 262144
-  node_exporter:
-    image: quay.io/prometheus/node-exporter:latest
-    container_name: node_exporter
-    command:
-      - '--path.rootfs=/host'
-    network_mode: host
-    pid: host
-    restart: unless-stopped
-    volumes:
-      - '/:/host:ro,rslave'
-EOF
+curl -s -H 'Metadata-Flavor: Google' 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/docker_compose' > compose.yml
 
 logi "execute docker compose up"
 docker compose up -d
