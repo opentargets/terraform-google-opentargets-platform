4a761126a18a1597e5340bb0af9ca85ac2fac907
ricardo <remo87@gmail.com>
2025-06-17 11:07:53 +0100

    update documentation variables and outputs

--- modules/prometheus/outputs.tf
+++ modules/prometheus/outputs.tf
@@ -11,13 +11,18 @@ output "map_region_to_instance_group_manager" {
 }
 
 output "prometheus_port" {
-  // Output the listening port for the Open Targets Platform API
+  // Output the listening port for the Open Targets Platform Prometheus
   value = local.otp_prometheus_port
 }
 
 output "prometheus_port_name" {
   // Output the custom named port for the instance group
   value = local.otp_prometheus_port_name
+}
+
+output "gramafa_port" {
+  // Output the listening port for the Open Targets Platform Grafana
+  value = local.otp_grafana_port
 }
 
 output "ilb_ip_addresses" {
